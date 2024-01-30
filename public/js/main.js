c41a17c84da39fb332406a43b6c087f8b42c38ed
Karapuchka <karapu4ka2@gmail.com>
2024-01-31 01:19:39 +0300

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 const burgerBtnExit = document.getElementById('js-burger-btn-exit');
 const burgerBtnOpen = document.getElementById('jd-burger-btn');
-const menu = document.getElementById('burder');
+const burger = document.getElementById('burder');
+const slider = document.getElementById('js-slider');
+const sliderLine = document.getElementById('js-slider-line');
+const sliderItems = document.getElementsByClassName('slider__line__item');
+const menu = document.getElementById('js-menu-list');
 
-if(!Boolean(menu.dataset.hidden)) {
+let activeItem = menu.children[0];
+
+activeItem.classList.add('menu__list__item--acrive');
+
+if(!Boolean(burger.dataset.hidden)) {
     burgerBtnOpen.style.opacity = '0';
     burgerBtnOpen.style.transform = 'translateX(-20%)';
-}
+};
+
+burger.style.height = window.innerHeight + 'px';
 
 burgerBtnExit.onclick = () => {
 
-    asideClouse(menu);
+    asideClouse(burger);
 
     setTimeout(()=>{
         burgerBtnOpen.style.opacity = '1';
@@ -18,7 +28,7 @@ burgerBtnExit.onclick = () => {
 
 burgerBtnOpen.onclick = () => {
 
-    asideClouse(menu);
+    asideClouse(burger);
 
     setTimeout(()=>{
         burgerBtnOpen.style.opacity = '0';
@@ -28,7 +38,7 @@ burgerBtnOpen.onclick = () => {
 function asideClouse(aside){
     if(!Boolean(aside.dataset.hidden)){
 
-        menu.style.transform = 'translateX(-150%)';
+        burger.style.transform = 'translateX(-150%)';
 
         aside.setAttribute('data-hidden', '1');
 
@@ -36,6 +46,26 @@ function asideClouse(aside){
 
     }  
     
-    menu.style.transform = 'translateX(0)';
+    burger.style.transform = 'translateX(0)';
     aside.setAttribute('data-hidden', '')
-}
+};
+
+for (let i = 0; i < sliderItems.length; i++) {
+    sliderItems[i].style.width = slider.offsetWidth + 'px';
+    sliderItems[i].children[0].style.width = slider.offsetWidth + 'px';
+};
+
+menu.onclick = (e)=>{
+
+    if(e.target.classList.contains('menu__list__item') && !e.target.classList.contains('menu__list__item--acrive')){
+        let show = -(+sliderItems[0].offsetWidth * +e.target.dataset.show) + 'px';
+
+        sliderLine.style.transform = `translateX(${show})`;
+
+        activeItem.classList.remove('menu__list__item--acrive');
+
+        e.target.classList.add('menu__list__item--acrive');
+
+        activeItem = e.target;
+    }
+};
